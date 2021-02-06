7cc79bc3aeb25c7e140b0a9d13269533935a7cb9
yamataikoku-maiko <78639158+yamataikoku-maiko@users.noreply.github.com>
2021-02-06 17:51:40 +0900

    Add files via upload

---
+++ taiwancurrent/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.signup-show').click(function(){
+        $('#signup-modal').fadeIn();
+    });
+
+    $('.close-modal').click(function(){
+        $('#signup-modal').fadeOut();
+    });
+
+});
